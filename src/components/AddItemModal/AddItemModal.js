eeb13999a1fc6d2dc465d54f9c3f06d1d3c8bb70
Zachary Wedel <133525592+zaccmagicdev@users.noreply.github.com>
2024-01-23 12:08:45 -0600

    Update AddItemModal.js

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ModalWithForm from '../ModalWithForm/ModalWithForm';
-import './AddItemModal.js';
+import './AddItemModal.css';
 
 function AddItemModal(props){
 
