610ac1999258bdc3886edd53ffba747a35f99438
Zachary Wedel <zwede710@gmail.com>
2024-09-19 14:47:33 -0500

    WIP: Making toggleswitches change on useState

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import './ToggleSwitch.css'
+import { set } from 'mongoose';
 
 function ToggleSwitch(props){
 
-  console.log(props.handleCallback)
+  const [isToggled, setToggled] = React.useState(false);
+  const togSwitch = React.useRef();
+
+ 
+
+  React.useEffect(() => {
+    isToggled ? togSwitch.checked = true : togSwitch.checked = false
+    props.handleCallback();
+  }, [isToggled])
 
   return (
     <div className='toggleswitch'>
@@ -11,7 +20,7 @@ function ToggleSwitch(props){
     <div className="container">
       <div className="toggle-switch">
         <input type="checkbox" className="checkbox"
-               name={props.label} id={props.label} onChange={props.handleCallback}/>
+               name={props.label} id={props.label} onChange={setToggled(true)} useRef={props.label} />
         <label className="label" htmlFor={props.label}>
           <span className="inner" />
           <span className="switch" />
